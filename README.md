c6471ee89db1ecd92c0354f69ca7ae918ff3c48a
Dickie Stevens <rsk0528@gmail.com>
2018-08-07 12:18:25 -0400

    commenting

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bamazon
-Amazon Site Using Node.js and MySQL
+Amazon replica site Using Node.js and MySQL
+
+#Instructions
+Select the Item ID of any product for sale, then select the quantity. 
+
+#MySQL Database Setup
+In order to run this application, you need to install the MySQL database to your machine. Once you have MySQL installed, create the Bamazon database and the products table with the SQL code found in Bamazon.sql. Run this code inside your MySQL. For this project I used MySQL Workbench, which can be found here: https://www.mysql.com/products/workbench/
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,35 +1,38 @@
+//getting our npm dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+//making our MySQL Connection
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
+    // Your port
     port: 3306,
-
-    // Your username
+    //MySQL Username
     user: "root",
-
-    // Your password
+    //MySQL Username password
     password: "NotAnother1",
+    //MySQL database we are connecting to
     database: "bamazon"
 });
 
+//confirming connection
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     readbamazonProducts();
 });
 
+//Grabing available products from MySQL
 function readbamazonProducts() {
-    //Grab the products from MySQL
+    //building mySQL query
     queryStr = 'SELECT * FROM products';
 
-    // Make the db query
+    //Grabbing and displaying the data
     connection.query(queryStr, function(err, data) {
     if (err) throw err;
     console.log("Here's what's in stock today: ");
-
+    
+    //displaying items in a fashionable manor
     var strOut = '';
     for (var i = 0; i < data.length; i++) {
         strOut = '';
@@ -38,15 +41,14 @@ function readbamazonProducts() {
         strOut += 'Department: ' + data[i].department_name + ' | ';
         strOut += 'Price: $' + data[i].price + ' | ';
         strOut += 'Stock Quantity: ' + data[i].stock_quantity + '\n';
-
         console.log(strOut);
     }
-        //console.log(res);
+        //running buy function
         buyProducts();
     })
 }
 
-//prompts the user for what action they should take
+//prompts the user for what action they should take (select items, purchase stuff, not purchase stuff, etc.)
 function buyProducts() {
     inquirer
     .prompt([
@@ -62,43 +64,113 @@ function buyProducts() {
         }
     ])
     .then(function(answer) {
-        // selectionConfirm(answer);
-                // based on their answer, either call the bid or the post functions
-                var item = answer.productNumber;
-                var quantity = answer.itemQuantity;
-                inquirer
-                .prompt( 
-                    {
-                        type: "confirm",
-                        message: "I see you've selected " + item + " and " + quantity + ". Is this correct?",
-                        name: "confirmlast",
-                        default: true
-                    }    
-                ).then(function(confirm) {
-                    if (confirm.default === true) {
-                        console.log("Ok kewl.");
-                    } else {
-                        console.log("No problem let's start over then.");
-                        buyProducts();
+        //running functions based on inquirer questions
+        var item = answer.productNumber;
+        var quantity = answer.itemQuantity;
+        var queryStr2 = 'SELECT * FROM products WHERE ?';
+        inquirer
+        .prompt( 
+            //confirm they selected the right item and quantity
+            {
+                type: "confirm",
+                message: "I see you've selected " + item + " and " + quantity + ". Is this correct?",
+                name: "confirmBuy",
+                default: true
+            }    
+        )
+        .then(function(userResponse) {
+            connection.query(queryStr2, {item_id: item}, function(err, data) {
+                if (err) throw err;
+                var productInfo = data[0];
+                //if user is ready to make the purchase
+                if (userResponse.confirmBuy) {
+                    console.log("Great.");
+                    //checking if we have enough inventory for purchase
+                    if(quantity <= productInfo.stock_quantity) {
+                        console.log("Looks like we have " + quantity + " " + productInfo.product_name + "(s) available."); 
+                        console.log("Your purchase was successful.");
+                        console.log("Thank you for using Bamazon, the #2 online retailer."); 
+                        connection.end();
+                    } 
+                    else {
+                        console.log("I'm sorry, we do not have " + quantity + " " + productInfo.product_name + "(s) available.");
+                        //in this case we do not have enough inventory.
+                        //checking if user would like to place another order.
+                        inquirer
+                        .prompt( 
+                            {
+                                type: "confirm",
+                                message: "Would you like to place another order?",
+                                name: "confirmTryAgain",
+                                default: true
+                            }
+                        )
+                        .then(function(repurchase) {
+                            //if user wants to make another purchase, start the buyProducts function.
+                            if(repurchase.confirmTryAgain) {
+                                console.log("Great. Let's start over.");
+                                buyProducts();
+                            //if they do not want to make another purchase, end the app.    
+                            } else {
+                                console.log("Sorry we could not complete your order today.");
+                                console.log("Thank you for using Bamazon, the #2 online retailer.");
+                                connection.end();
+                            }
+                        });        
+                        
                     }
-                });
+                  //if the user selected the wrong item or quantity and confirmed so
+                  //start over by running the buyProducts function.     
+                } else {
+                    console.log("No problem let's start over then.");
+                    buyProducts();
+                }
             });
+        });
+    });
 }
 
-// function selectionConfirm() {
-//         // based on their answer, either call the bid or the post functions
-//         var item = answer.productnumber;
-//         var quantity = answer.itemQuantity;
-//         inquirer
-//         .prompt( 
-//             {
-//                 type: "confirm",
-//                 message: "I see you've selected " + item + " and " + quantity + ". Is this correct?",
-//                 name: "confirmlast",
-//                 default: true
-//             }    
-//         ).then(console.log("We outta dat."));
-// }
 
+///////////////////////////////////////////////////////////////////
+// ]).then(function(input) {
+//     // console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
 
-  //connection.end();
+//     var item = input.item_id;
+//     var quantity = input.quantity;
+
+//     // Query db to confirm that the given item ID exists in the desired quantity
+//     var queryStr = 'SELECT * FROM products WHERE ?';
+
+//     connection.query(queryStr, {item_id: item}, function(err, data) {
+//       if (err) throw err;
+
+//       // If the user has selected an invalid item ID, data will be empty
+//       // console.log('data = ' + JSON.stringify(data));
+
+//       if (data.length === 0) {
+//         console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+//         displayInventory();
+
+//       } else {
+//         var productData = data[0];
+//         // If the quantity requested by the user is in stock
+//         if (quantity <= productData.stock_quantity) {
+//           console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+//           // Construct the updating query string
+//           var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+//           // console.log('updateQueryStr = ' + updateQueryStr);
+
+//           // Update the inventory
+//           connection.query(updateQueryStr, function(err, data) {
+//             if (err) throw err;
+
+//             console.log('Your order has been placed! Your total is $' + productData.price * quantity);
+//             console.log('Thank you for shopping with us!');
+//             console.log("\n---------------------------------------------------------------------\n");
+
+//             // End the database connection
+//             connection.end();
+//           })
+
+//   //connection.end();
